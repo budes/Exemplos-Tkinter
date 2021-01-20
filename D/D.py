c22576311b3f40a325b6d6b6de75955ea31415ee
budes <74151920+budes@users.noreply.github.com>
2021-01-19 22:04:34 -0300

    Add files via upload

--- D/D.py
+++ D/D.py
@@ -1,17 +1,20 @@
 from tkinter import *
 from functools import partial
+from PIL import Image, ImageTk 
+
 
 class Calculadora():
 	def __init__(self):
-		fonte = ('Verdana', 12, 'bold')
-		fontea = ('Verdana', 12)
-		
-		fonte2 = ('Verdana', 18, 'bold')
 		
 		# Coloquei a instancia Tk e o background
 		self.inst = Tk()
 		self.inst.geometry('720x1200')
 		self.inst['background'] = 'white'
+		
+		# Fontes e imagens usadas
+		fonte = ('Verdana', 12, 'bold')
+		fontea = ('Verdana', 12)
+		fonte2 = ('Verdana', 18, 'bold')
 	
 		# Vou colocar os calculos no Label
 		self.calculo = Label(self.inst, text='', font=fonte2,
@@ -26,27 +29,31 @@ class Calculadora():
 		for empacotar in Frames: empacotar.pack()
 		
 		self.texto = (
-		('C', '<×', '**', '/'),
-		('7', '8', '9', '*'),
+		('C', '<×', '^', '/'),
+		('7', '8', '9', 'x'),
 		('4', '5', '6', '+'),
 		('1', '2', '3', '-'),
-		('.', '0', '÷', '=')
+		('.', '0', '()', '=')
 		)
 		
 		# Cria os botoes na tela
-		botoes = []
+		self.botoes = []
 		
 		for i in range(5):
 			frame = Frames[i]
 			for simbol in self.texto[i]:
+				
 				but = Button(frame, text=simbol, font=fontea, 
 				height=2, width=3, relief=GROOVE, bg='white',
 				command=partial(self.InterpretaBotoes, simbol)
 				)
+					
+				but.pack(side=LEFT)
+				self.botoes.append(but)
 				
 				# Muda a cor e a fonte do botão de acordo com o tipo
 				
-				if simbol in ('C', '<×', '**', '/', '*', '+', '-'):
+				if simbol in ('C', '<×', '^', '/', 'x', '+', '-'):
 					but['bg'] = 'lightgray'
 					but['fg'] = 'darkcyan'
 					but['font'] = fonte
@@ -55,22 +62,32 @@ class Calculadora():
 					but['bg'] = 'green'
 					but['fg'] = 'white'
 					but['font'] = fonte
-					
-				
-				but.pack(side=LEFT)
-				botoes.append(but)
+			
 		
 		# Inicia a instancia
 		self.inst.mainloop()
 		
 	# Vai executar o comando do botão pressionado
 	def InterpretaBotoes(self, valor):
+		
 		if valor == 'C':
 			self.calculo['text'] = ''
 		elif valor == '<×':
 			self.calculo['text'] = self.calculo['text'][:len(self.calculo['text'])-1]
 		elif valor == '=':
 			self.Calcula()
+		elif valor == '()':
+			texto = self.calculo['text']
+			try:
+				if texto[len(texto)-1] in '+-/^x' or len(texto) == 0:
+					self.calculo['text'] += '('
+					
+				elif texto[len(texto)-1] in '1234567890)':
+					self.calculo['text'] += ')'
+					
+			except:
+				self.calculo['text'] += '('
+			
 		else:
 			self.calculo['text'] += valor
 			
@@ -79,20 +96,37 @@ class Calculadora():
 		
 	
 	def Calcula(self):
-		print(any([op in self.calculo['text'] for op in ['+', '-', '/', '**', '*']]))
-		print([op in self.calculo['text'] for op in ['+', '-', '/', '**', '*']])
-		
 		# Se tiver um dos operadores ele tenta fazer calculo
-		if any([op in self.calculo['text'] for op in ['+', '-', '/', '**', '*']]):
+		if any([op in self.calculo['text'] for op in ['+', '-', '/', '^', 'x']]):
 			calculo = ''
 			for elemento in self.calculo['text']:
 				for e in self.texto:
-					if elemento in e:
+					if elemento == 'x':
+						calculo += '*'
+						break
+						
+					elif elemento == '^':
+						calculo += '**'
+						break
+						
+					elif elemento in '()':
 						calculo += elemento
+						break 
+						
+					elif elemento in e:
+						calculo += elemento
+						break
 				
-			calculo = eval(calculo)
-			self.calculo['text'] = str(calculo)
+			self.calculo['text'] = ''
+			resultado = str(eval(calculo))
 			
-		
+			auxiliar = ''
+			for i in range(len(resultado)):
+				if (i+1) % 15 == 0:
+					auxiliar += '\n'
+					
+				auxiliar += resultado[i]
+			
+			self.calculo['text'] = auxiliar
 		
 Calculadora()
